4e645d0c1c9e3f60894a8e1643a83ca6e3e283f0
Tomasz Lewicki <t.w.lewicki@gmail.com>
2020-03-28 16:41:12 -0700

    updated deploy rgb

--- deploy_rgb.sh
+++ deploy_rgb.sh
@@ -1,1 +1,2 @@
-gst-launch-1.0 nvarguscamerasrc sensor_mode=0 ! 'video/x-raw(memory:NVMM),width=3264, height=2464, framerate=21/1, format=NV12' ! nvvidconv flip-method=2 ! 'video/x-raw,width=1440, height=1080' ! omxh264enc ! qtmux ! filesink location=RGB.mp4 -e
+gst-launch-1.0 nvarguscamerasrc sensor_mode=0 ! 'video/x-raw(memory:NVMM),width=3264, height=2464, framerate=21/1, format=NV12' ! nvvidconv flip-method=2  ! nvvidconv top=96 bottom=2368 left=137 right=3127 ! 'video/x-raw(memory:NVMM),width=1440, height=1080' ! omxh264enc ! qtmux ! filesink location=RGB.mp4 -e
+
