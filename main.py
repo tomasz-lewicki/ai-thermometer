9d754093d3fcf76fe9ce421dad255f20c7e582ec
Tomek Lewicki <t.w.lewicki@gmail.com>
2020-09-09 03:25:34 -0700

    updated v3.0 with temperature overlay

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from threading import Thread
 from queue import Queue
 from concurrent.futures import ThreadPoolExecutor
@@ -10,46 +9,66 @@ import time
 from ir import IRThread
 from vis import GPUThread
 
-from ir.utils import overlay_bboxes as overlay_ir_bboxes 
-from vis.utils import draw_boxes as overlay_vis_bboxes 
+from ir.utils import overlay_bboxes as overlay_ir_bboxes
+from vis.utils import draw_boxes as overlay_vis_bboxes, overlay_temps
+
+import numpy as np
+
+
+def draw_rectangle(arr):
+    center = np.array([0.5, 0.5])
+    wh = np.array([0.5, 0.5])
+
+    p1 = np.array([center[0] - wh[0] / 2, center[1] - wh[1] / 2])
+
+    p2 = np.array([center[0] + wh[0] / 2, center[1] + wh[1] / 2])
+
+    # scale it to image size
+    s = np.array(arr.shape[:2][::-1])
+
+    p1 = tuple(np.array(p1 * s, dtype=np.int))
+    p2 = tuple(np.array(p2 * s, dtype=np.int))
+
+    cv2.rectangle(arr, p1, p2, (255, 255, 255), 3)
+
 
 def exit_handler():
     print("exit handler called")
     gpu_thread.stop()
     ir_thread.stop()
-    
+
     gpu_thread.join()
     ir_thread.join()
-    
+
     cv2.destroyAllWindows()
 
+
 if __name__ == "__main__":
-    
-    MAIN_MIN_LATENCY = 1/20 # run main thread at ~20Hz
+
+    MAIN_MIN_LATENCY = 1 / 20  # run main thread at ~20Hz
 
     DISPLAY = True
-    WIN_SIZE = (800, 600)
-    FACE_BB_COLOR = (255, 255, 255) # white
-    EYES_BB_COLOR = (0,   255, 255) # yellow
+    WIN_SIZE = (600, 450)
+    FACE_BB_COLOR = (255, 255, 255)  # white
+    EYES_BB_COLOR = (0, 255, 255)  # yellow
     LOG_DIR = "logs"
 
     APP_NAME = "AI Thermometer"
     IR_WIN_NAME = APP_NAME + ": IR frame"
-    VIS_WIN_NAME = APP_NAME + ": VIS frame" 
-
+    VIS_WIN_NAME = APP_NAME + ": VIS frame"
 
     gpu_thread = GPUThread(frame_size=WIN_SIZE)
     gpu_thread.start()
 
-    ir_thread = IRThread()
+    ir_thread = IRThread(resize_to=WIN_SIZE)
     ir_thread.start()
 
     executor = ThreadPoolExecutor(max_workers=4)
 
     cv2.namedWindow(VIS_WIN_NAME)
     cv2.namedWindow(IR_WIN_NAME)
-    cv2.moveWindow(IR_WIN_NAME, WIN_SIZE[0], 0)
-    
+    cv2.moveWindow(IR_WIN_NAME, 0, WIN_SIZE[1])
+
     try:
         while gpu_thread.frame is None:
             print("Waiting for RGB frames")
@@ -64,30 +83,37 @@ if __name__ == "__main__":
 
             time_start = time.monotonic()
 
-            ir_frame_w_overlay = overlay_ir_bboxes(ir_thread.frame, ir_thread.bboxes)
-            vis_frame_w_overlay = overlay_vis_bboxes(gpu_thread.frame, gpu_thread.detections)
+            ir_arr = ir_thread.frame
+            temps = ir_thread.temperatures
+
+            rgb_arr = gpu_thread.frame
+            dets = gpu_thread.detections
+
+            rgb_arr_o = overlay_vis_bboxes(rgb_arr, dets)
+            ir_arr_o = overlay_temps(rgb_arr, ir_arr, dets, temps)
+
+            # draw rectangle to show the approx. IR overlay on VIS frame
+            draw_rectangle(rgb_arr_o)
 
             # Show
-            cv2.imshow(VIS_WIN_NAME, vis_frame_w_overlay)
-            cv2.imshow(IR_WIN_NAME, ir_frame_w_overlay)
+            cv2.imshow(VIS_WIN_NAME, rgb_arr_o)
+            cv2.imshow(IR_WIN_NAME, ir_arr_o)
             key = cv2.waitKey(1) & 0xFF
 
             # Save frames
-            executor.submit(cv2.imwrite, f"{LOG_DIR}/frames/vis/{i:05d}.jpg", vis_frame_w_overlay)
-            executor.submit(cv2.imwrite, f"{LOG_DIR}/frames/ir/{i:05d}.jpg", ir_frame_w_overlay)
-            
+            executor.submit(cv2.imwrite, f"{LOG_DIR}/frames/vis/{i:05d}.jpg", rgb_arr_o)
+            executor.submit(cv2.imwrite, f"{LOG_DIR}/frames/ir/{i:05d}.jpg", ir_arr)
+
             # if the `q` key was pressed in the cv2 window, break from the loop
             if key == ord("q"):
                 break
 
-            main_latency =  time.monotonic() - time_start
-            print(f"GPU thread latency={gpu_thread._delay:.2f}    IR thread latency={ir_thread.latency:.2f}      Main thread latency={1000 * main_latency:.2f}")
-            
+            main_latency = time.monotonic() - time_start
+            print(
+                f"GPU thread latency={gpu_thread._delay:.2f}    IR thread latency={ir_thread.latency:.2f}      Main thread latency={1000 * main_latency:.2f}"
+            )
+
             time.sleep(max(0, MAIN_MIN_LATENCY - main_latency))
 
     finally:
         exit_handler()
-
-
-
-
--- vis/utils.py
+++ vis/utils.py
@@ -1,25 +1,26 @@
 import numpy as np
 import cv2
 
+
 def draw_boxes(arr, detections):
-    
+
     if detections is None:
         return arr
 
     h, w = arr.shape[:2]
-    scores = detections[:,2]
-    
+    scores = detections[:, 2]
+
     # loop over the detections
     for (_, _, score, x1, y1, x2, y2) in detections[scores > 0.5]:
-        
+
         # scale box
         box = np.array([x1, y1, x2, y2]) * np.array([w, h, w, h])
-        
+
         # cast to int
         (x1, y1, x2, y2) = box.astype("int")
-        
+
         # draw box
-        cv2.rectangle(arr, (x1, y1), (x2, y2),(0, 0, 255), 2)
+        cv2.rectangle(arr, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
         # put text
         cv2.putText(
@@ -29,7 +30,92 @@ def draw_boxes(arr, detections):
             cv2.FONT_HERSHEY_SIMPLEX,
             0.45,
             (0, 0, 255),
-            2
+            2,
         )
 
     return arr
+
+
+def img2euc(x, y):
+    """
+    image frame to Euclidean frame
+    """
+    x = x - 0.5
+    y = -y + 0.5
+    return x, y
+
+
+def euc2img(x, y):
+    """
+    Euclidean frame to image frame
+    """
+    x = x + 0.5
+    y = -y + 0.5
+    return x, y
+
+
+def shift(x, y, dx, dy):
+    x += dx
+    y += dy
+    return x, y
+
+
+def overlay_temps(rgb_arr, ir_arr, detections, temperatures):
+
+    # scaling width, scaling height
+    SW, SH = 0.5, 0.5
+
+    if detections is None:
+        return ir_arr
+
+    h_rgb, w_rgb = rgb_arr.shape[:2]
+    h_ir, w_ir = ir_arr.shape[:2]
+
+    scores = detections[:, 2]
+
+    # loop over the detections
+    for (_, _, score, x1, y1, x2, y2) in detections[scores > 0.5]:
+
+        # box in euclidean rgb frame
+        x1, y1 = img2euc(x1, y1)
+        x2, y2 = img2euc(x2, y2)
+
+        # scale to convert to ir frame
+        x1 /= SW
+        x2 /= SW
+        y1 /= SH
+        y2 /= SH
+
+        # shift right
+        x1, y1 = shift(x1, y1, -0.075, 0)
+        x2, y2 = shift(x2, y2, -0.075, 0)
+
+        # convert back to image frame
+        x1, y1 = euc2img(x1, y1)
+        x2, y2 = euc2img(x2, y2)
+
+        # scale
+        box = np.array([x1, y1, x2, y2])
+        box *= np.array([w_ir, h_ir, w_ir, h_ir])
+
+        # cast to int
+        (x1, y1, x2, y2) = box.astype("int")
+
+        # draw box
+        cv2.rectangle(ir_arr, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+        roi = temperatures[x1:x2, y1:y2]
+        temp = np.mean(roi[roi > 32])
+
+        # put text
+        cv2.putText(
+            ir_arr,
+            f"{temp:.2f} C",
+            (x1, y1 - 10 if y1 > 20 else y1 + 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.45,
+            (0, 0, 255),
+            2,
+        )
+
+    return ir_arr
