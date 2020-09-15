ed6d71e913f0dc1f8ac7bc3ef0a1fa0c9e374d1e
Tomek Lewicki <t.w.lewicki@gmail.com>
2020-09-15 01:22:28 -0700

    moved UI functions to a separate module

--- ui/inteface.py
+++ ui/inteface.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2
+from .transforms import img2euc, euc2img, shift
 
 
-def draw_boxes(arr, detections):
+def make_rgb_view(arr, detections):
 
     if detections is None:
         return arr
@@ -11,7 +12,7 @@ def draw_boxes(arr, detections):
     scores = detections[:, 2]
 
     # loop over the detections
-    for (_, _, score, x1, y1, x2, y2) in detections[scores > 0.5]:
+    for (_, _, score, x1, y1, x2, y2) in detections[scores > 0.2]: #TODO: put the thresholding outside 
 
         # scale box
         box = np.array([x1, y1, x2, y2]) * np.array([w, h, w, h])
@@ -35,32 +36,26 @@ def draw_boxes(arr, detections):
 
     return arr
 
+def normalize_ir(ir_arr):
+    ir_arr_normalized = cv2.normalize(ir_arr, None, 0, 255, cv2.NORM_MINMAX)  # normalize to 0-255
+    ir_arr_normalized = ir_arr_normalized.astype(np.uint8)  # convert to uint8
+    return ir_arr_normalized
 
-def img2euc(x, y):
-    """
-    image frame to Euclidean frame
-    """
-    x = x - 0.5
-    y = -y + 0.5
-    return x, y
+def apply_cmap(ir_arr, threshold=36):
 
+    ir_arr_n = normalize_ir(ir_arr)
+    ir_arr_n = ir_arr_n.astype(np.uint8)
+    arr_3ch = cv2.cvtColor(ir_arr_n, cv2.COLOR_GRAY2BGR)
 
-def euc2img(x, y):
-    """
-    Euclidean frame to image frame
-    """
-    x = x + 0.5
-    y = -y + 0.5
-    return x, y
+    ir_arr_n = cv2.applyColorMap(ir_arr_n, cv2.COLORMAP_JET)
 
+    mask = ir_arr<threshold
+    mask = np.stack(3*[mask], axis=-1)
 
-def shift(x, y, dx, dy):
-    x += dx
-    y += dy
-    return x, y
+    return np.where(mask, arr_3ch, ir_arr_n)
 
 
-def overlay_temps(rgb_arr, ir_arr, detections, temperatures):
+def make_ir_view(rgb_arr, ir_arr, detections, temperatures):
 
     # scaling width, scaling height
     SW, SH = 0.5, 0.5
@@ -73,9 +68,12 @@ def overlay_temps(rgb_arr, ir_arr, detections, temperatures):
 
     scores = detections[:, 2]
 
-    # loop over the detections
-    for (_, _, score, x1, y1, x2, y2) in detections[scores > 0.5]:
+    ir_arr_3ch = apply_cmap(temperatures)
 
+    # loop over the detections
+    for (_, _, score, x1, y1, x2, y2) in detections[scores > 0.2]: #TODO: adjustable threshold
+
+        # TODO: interface should only receive the detections after transform!
         # box in euclidean rgb frame
         x1, y1 = img2euc(x1, y1)
         x2, y2 = img2euc(x2, y2)
@@ -86,9 +84,9 @@ def overlay_temps(rgb_arr, ir_arr, detections, temperatures):
         y1 /= SH
         y2 /= SH
 
-        # shift right
-        x1, y1 = shift(x1, y1, -0.075, 0)
-        x2, y2 = shift(x2, y2, -0.075, 0)
+        # shift left
+        x1, y1 = shift(x1, y1, +0.05, 0)
+        x2, y2 = shift(x2, y2, +0.05, 0)
 
         # convert back to image frame
         x1, y1 = euc2img(x1, y1)
@@ -102,14 +100,14 @@ def overlay_temps(rgb_arr, ir_arr, detections, temperatures):
         (x1, y1, x2, y2) = box.astype("int")
 
         # draw box
-        cv2.rectangle(ir_arr, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        cv2.rectangle(ir_arr_3ch, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
         roi = temperatures[x1:x2, y1:y2]
         temp = np.mean(roi[roi > 32])
 
         # put text
         cv2.putText(
-            ir_arr,
+            ir_arr_3ch,
             f"{temp:.2f} C",
             (x1, y1 - 10 if y1 > 20 else y1 + 10),
             cv2.FONT_HERSHEY_SIMPLEX,
@@ -118,4 +116,4 @@ def overlay_temps(rgb_arr, ir_arr, detections, temperatures):
             2,
         )
 
-    return ir_arr
+    return ir_arr_3ch
