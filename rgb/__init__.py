6ad3c5001132683838e9b69cf5dab494d6dc7f52
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-01-18 21:55:09 -0800

    Use cv2 capture object from cameras submodule

--- rgb/__init__.py
+++ rgb/__init__.py
@@ -3,50 +3,7 @@ import time, os
 import numpy as np
 import cv2
 from .ssd import SsdDetector
-
-
-def imx219_pipeline(
-    capture_width=3264,
-    capture_height=2464,
-    display_width=1024,
-    display_height=768,
-    framerate=10,
-    flip_method=2,
-):
-    return (
-        "nvarguscamerasrc ! "
-        "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! "
-        "appsink max-buffers=1 drop=true"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
-
-
-def make_vis_stream(display_width=1088, display_height=816):
-
-    pipeline = imx219_pipeline(
-        capture_width=3264,
-        capture_height=2464,
-        display_width=display_width,
-        display_height=display_height,
-        framerate=21,
-        flip_method=2,
-    )
-
-    return cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
-
+from .camera import make_imx219_capture
 
 
 class GPUThread(Thread):
@@ -62,10 +19,7 @@ class GPUThread(Thread):
             + "/ssd/caffe/res10_300x300_ssd_iter_140000.caffemodel",
         )
 
-        if stream is None:
-            stream = make_vis_stream(*frame_size)
-
-        self._stream = stream
+        self._stream = make_imx219_capture(0)
         self._delay = -1
         self._detections = None
         self._running = True
