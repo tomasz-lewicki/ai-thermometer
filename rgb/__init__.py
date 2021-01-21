9e21e9c75211b59b38f459a72b8d24e134f73473
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-01-21 00:31:52 -0800

    Support landmarks + mult. detectors in rgb_thread

--- rgb/__init__.py
+++ rgb/__init__.py
@@ -1,54 +1,55 @@
+import time
 from threading import Thread
-import time, os
-import numpy as np
+
 import cv2
-from .ssd import SsdDetector
+import numpy as np
+
 from .camera import make_imx219_capture
+from .retinaface import RetinaFaceDetector
+from .ssd import SsdDetector
 
 
 class RGBThread(Thread):
-    def __init__(self, stream=None, frame_size=(1088, 816)):
+    def __init__(self, model="SSD"):
 
         super(RGBThread, self).__init__()
 
-        parent_dir_pth = os.path.dirname(os.path.abspath(__file__))
-
-        self._detector = SsdDetector(
-            prototxt_file_pth=parent_dir_pth + "/ssd/caffe/deploy.prototxt.txt",
-            caffe_model_pth=parent_dir_pth
-            + "/ssd/caffe/res10_300x300_ssd_iter_140000.caffemodel",
-        )
+        if model == "SSD":
+            self._detector = SsdDetector()
+        elif model == "retinaface":
+            self._detector = RetinaFaceDetector()
+        else:
+            raise ValueError(f"{self._detector} is not a valid argument.")
 
         self._stream = make_imx219_capture(0)
         self._delay = -1
-        self._detections = None
+        self._scores, self._boxes, self._landms = [], [], []
         self._running = True
-        self._frame = None
+        self._frame = np.zeros((1024, 768), np.uint8)
+        self._frame_small = np.zeros((400, 300), np.uint8)
 
     def run(self):
 
         while self._running:
+
             loop_start = time.monotonic()
             ret, self._frame = self._stream.read()
-            # ret, _frame = self._stream.retrieve()
+
+            frame_small = cv2.resize(self._frame, (400, 300))
 
             if ret is False:
                 print("WARN: no frame")
                 time.sleep(1)
                 continue
 
-            self._detections = self._detector(self._frame)
+            self._scores, self._boxes, self._landms = self._detector(frame_small)
             self._delay = 1000 * (time.monotonic() - loop_start)
 
         print("releasing")
         self._stream.release()
 
-    def get_faces(self):
-        """
-        :ret list of x,y positions of faces in image _frame:
-        """
-
-        return self._detections
+    def get_detections(self):
+        return self._scores, self._boxes, self._landms
 
     def stop(self):
         self._running = False
