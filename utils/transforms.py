8b424a90a36c4b9baa6384ce13c7bfc899ee5243
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-01-21 00:40:22 -0800

    Add transform_boxes method

--- utils/transforms.py
+++ utils/transforms.py
@@ -17,6 +17,40 @@ def euc2img(x, y):
 
 
 def shift(x, y, dx, dy):
+    """
+    shift x,y by (dx,dy) vector
+    """
     x += dx
     y += dy
     return x, y
+
+
+def transform_boxes(bboxes, scale_x=1, scale_y=1, shift_x=0, shift_y=0):
+
+    bboxes_out = []
+
+    for i, bbox in enumerate(bboxes):
+
+        x1, y1, x2, y2 = bbox
+
+        # img -> euclidean
+        x1, y1 = img2euc(x1, y1)
+        x2, y2 = img2euc(x2, y2)
+
+        # scale
+        x1 *= scale_x
+        x2 *= scale_x
+        y1 *= scale_y
+        y2 *= scale_y
+
+        # shift left
+        x1, y1 = shift(x1, y1, shift_x, shift_y)
+        x2, y2 = shift(x2, y2, shift_x, shift_y)
+
+        # convert back to image frame
+        x1, y1 = euc2img(x1, y1)
+        x2, y2 = euc2img(x2, y2)
+
+        bboxes_out.append([x1, y1, x2, y2])
+
+    return np.array(bboxes_out)
